0036ecc1981c3a01bd4f6970881720d2fb8b6a82
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-05-03 23:13:17 +0200

    Limit Get-PackageProvider output properties

--- modules/Gucu112.Powershell.PackageManagement/tools/InstallPackageManagement.ps1
+++ modules/Gucu112.Powershell.PackageManagement/tools/InstallPackageManagement.ps1
@@ -8,6 +8,8 @@ if (Find-Module Gucu112.Powershell.PackageManagement -ErrorAction Ignore) {
     Import-Module (Join-Path $PSScriptRoot '..\Gucu112.Powershell.PackageManagement.psd1')
 }
 
+$packageProviderProperties = (Get-Module Gucu112.Powershell.PackageManagement).PrivateData.Configuration.PackageProviderProperties
+
 # Get-AvailablePackageProvider
 Get-PackageProvider -ListAvailable
 
@@ -19,7 +21,7 @@ Get-PSRepository -Name 'PSGallery'
 ###
 
 # Add-PSPackageProvider
-Add-PackageProvider -Name 'PowerShellGet' -Force -ErrorAction Stop
+Add-PackageProvider -Name 'PowerShellGet' -Force -ErrorAction Stop | Select-Object -Property $packageProviderProperties
 
 # Add-PSPackageSource
 Add-PackageSource -Name 'PSGallery' -ProviderName 'PowerShellGet' -Location 'https://www.powershellgallery.com/api/v2' -Trusted -Force
@@ -32,7 +34,7 @@ Install-Module -Name 'dbatools'
 ###
 
 # Add-NuGetPackageProvider
-Add-PackageProvider -Name 'NuGet' -Force -ErrorAction Stop
+Add-PackageProvider -Name 'NuGet' -Force -ErrorAction Stop | Select-Object -Property $packageProviderProperties
 
 # Add-NuGetPackageSource -Default
 Add-PackageSource -Name 'NuGetGallery' -ProviderName 'NuGet' -Location 'https://www.nuget.org/api/v2' -Trusted -Force
@@ -45,7 +47,7 @@ Install-Package -Name 'ConfigurationHelper' -ProviderName 'NuGet' -Destination '
 ###
 
 # Add-ChocolateyPackageProvider
-Add-PackageProvider -Name 'ChocolateyGet' -Force -ErrorAction Stop
+Add-PackageProvider -Name 'ChocolateyGet' -Force -ErrorAction Stop | Select-Object -Property $packageProviderProperties
 
 # Add-ChocolateyPackageSource -Default
 Add-PackageSource -Name 'Chocolatey' -ProviderName 'ChocolateyGet' -Location 'https://www.chocolatey.org/api/v2' -Trusted -Force
