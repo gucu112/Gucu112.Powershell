8d68b75e249cee25bce902cae40fefbba3368f07
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-05-06 07:07:44 +0200

    Add New-ShortcutItem function

--- others/WindowsInstallation/28_WindowsTaskbar.ps1
+++ others/WindowsInstallation/28_WindowsTaskbar.ps1
@@ -1,1 +1,43 @@
 # https://superuser.com/questions/1193985/command-line-code-to-pin-program-to-taskbar-windows-10
+
+# %AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar
+
+function New-ShortcutItem {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory)]
+        [string]$TargetPath,
+
+        # [Parameter]
+        [string]$Arguments = $null,
+
+        # [Parameter]
+        [string]$WorkingDirectory = ($TargetPath | Split-Path),
+
+        # [Parameter]
+        [string]$IconFilePath = $TargetPath
+
+        # [Parameter]
+        # [switch] $Remove
+    )
+
+    $wshShell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
+    $menuStart = $wshShell.SpecialFolders.Item('Programs')
+    $shortcut = $wshShell.CreateShortcut((Join-Path $menuStart "$Name.lnk"))
+
+    # If ($Remove.IsPresent) {
+    #     $shortcut.FullName | Remove-Item
+    #     Exit
+    # }
+
+    $shortcut.TargetPath = $TargetPath
+    $shortcut.Arguments = $Arguments
+    $shortcut.IconLocation = $IconFilePath
+    $shortcut.Description = $null
+    $shortcut.WorkingDirectory = $WorkingDirectory
+    $shortcut.Save()
+}
+
+New-ShortcutItem -Name 'WinMerge Test' -TargetPath "C:\Users\BasowQA\AppData\Local\Programs\WinMerge\WinMergeU.exe"
