30f6db80e089643c986a6d2627bc923684979a3f
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-05-20 14:08:32 +0200

    Check rename user script

--- others/WindowsInstallation/03_Rename_User.ps1
+++ others/WindowsInstallation/03_Rename_User.ps1
@@ -1,5 +1,8 @@
-# TODO: Check if working and if requires elevated privileges
+# TODO: Try to elevate administration rights
+
 $firstLocalUser = Get-LocalUser | Where-Object Enabled | Select-Object -First 1
 $firstLocalUser | Rename-LocalUser -NewName 'PC'
+
+# TODO: Try to rename user folder as well
 
 # Restart-Computer
