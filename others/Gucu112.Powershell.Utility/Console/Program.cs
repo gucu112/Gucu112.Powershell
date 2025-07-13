a38fc20e91ec33b1d7c3b12efcd819d894e2c32e
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-07-13 22:55:09 +0200

    Add comment about system identity

--- others/Gucu112.Powershell.Utility/Console/Program.cs
+++ others/Gucu112.Powershell.Utility/Console/Program.cs
@@ -3,6 +3,8 @@ using Gucu112.Powershell.Utility.Windows;
 Console.WriteLine("GetSystem()");
 var systemProcess = Process.GetSystem();
 Console.WriteLine($"System Process: {systemProcess?.Id} - {systemProcess?.ProcessName}");
+
+// REMARK: Can't take the token of the system process directly due to access restrictions
 //var systemToken = new Token(systemProcess);
 //var systemIdentity = systemToken.GetWindowsIdentity();
 //Console.WriteLine($"System Identity: {systemIdentity?.Name} - {systemIdentity?.User} - {systemIdentity?.AuthenticationType}");
@@ -20,10 +22,3 @@ Console.WriteLine($"Current Process: {currentProcess?.Id} - {currentProcess?.Pro
 var currentToken = new Token(currentProcess);
 var currentIdentity = currentToken.GetWindowsIdentity();
 Console.WriteLine($"Current Identity: {currentIdentity?.Name} - {currentIdentity?.User} - {currentIdentity?.AuthenticationType}");
-
-// TODO: Add DLLs to required assemblies of the module
-// RequiredAssemblies = @(
-//     '..\bin\Gucu112.Powershell.Utility.Win32.dll',
-//     '..\bin\Gucu112.Powershell.Utility.WindowsProcess.dll',
-//     '..\bin\Gucu112.Powershell.Utility.WindowsToken.dll',
-// )
