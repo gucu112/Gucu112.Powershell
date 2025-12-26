7fc060521d2271e2b99174b3a8759232f99227af
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-12-26 23:19:53 +0100

    Update rename computer script

--- others/WindowsInstallation/02_Rename_Computer.ps1
+++ others/WindowsInstallation/02_Rename_Computer.ps1
@@ -1,6 +1,22 @@
-# TODO: Try to elevate administration rights
+param(
+    [string]$ComputerName = "Gucu112-LAPTOP"
+)
 
-# Run as administrator
-Rename-Computer -NewName "Gucu112-LAPTOP"
+Import-Module (Join-Path $PSScriptRoot '..\..\modules\Gucu112.Powershell.Utility\Gucu112.Powershell.Utility.psd1')
 
-#Restart-Computer
+if (-not (Test-WindowsIdentity -Administrator)) {
+    Start-Process -FilePath "powershell" -Verb "RunAs" `
+        -ArgumentList @("-File", $($MyInvocation.MyCommand.Path))
+    exit
+}
+
+if ((Get-ComputerInfo).CsDnsHostName -ne $ComputerName) {
+    Rename-Computer -NewName $ComputerName
+    Write-Information "Computer will be restarted in 5 seconds..."
+    Start-Sleep -Seconds 5
+    Restart-Computer
+}
+else {
+    Write-Warning "The computer name is already set to '$ComputerName'."
+    Start-Sleep -Seconds 5
+}
