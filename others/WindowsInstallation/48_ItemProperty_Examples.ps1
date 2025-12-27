66a6fbece456a02b5c9cf9c68f54f63fbf2be466
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-12-27 11:26:25 +0100

    Uncomment code in ItemProperty example script

--- others/WindowsInstallation/48_ItemProperty_Examples.ps1
+++ others/WindowsInstallation/48_ItemProperty_Examples.ps1
@@ -1,14 +1,14 @@
-#Write-Host 'Get registry hive'
-#Get-ChildItem $HKCU_Search
+Write-Host 'Get registry hive'
+Get-ChildItem $HKCU_Search
 
-#Write-Host 'Get registry key'
-#Get-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode'
+Write-Host 'Get registry key'
+Get-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode'
 
-#Write-Host 'New registry key'
-#New-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -PropertyType DWord -Value 0
+Write-Host 'New registry key'
+New-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -PropertyType DWord -Value 0
 
-#Write-Host 'Set registry key'
-#Set-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -Value 2
+Write-Host 'Set registry key'
+Set-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -Value 2
 
-#Write-Host 'Add registry key'
-#Add-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -Value 2
+Write-Host 'Add registry key'
+Add-ItemProperty $HKCU_Search -Name 'SearchboxTaskbarMode' -Value 2
