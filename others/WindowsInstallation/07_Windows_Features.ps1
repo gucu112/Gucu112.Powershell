15c51cc9132115cd00530bbbc24ab0f0bfd69310
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-12-27 12:53:51 +0100

    Add scripts to install hypervisor and linux subsystem

---
+++ others/WindowsInstallation/07_Windows_Features.ps1
@@ -0,0 +1,11 @@
+# requires administration privileges
+Get-WindowsOptionalFeature -Online | Format-Table
+
+Disable-WindowsOptionalFeature -FeatureName 'WindowsMediaPlayer' -Online
+
+$hypervisorFeatures = @(
+    'HypervisorPlatform'
+    'VirtualMachinePlatform'
+    'Microsoft-Windows-Subsystem-Linux'
+)
+Enable-WindowsOptionalFeature -FeatureName $hypervisorFeatures -Online -NoRestart
---
+++ others/WindowsInstallation/08_Linux_SubSystem.ps1
@@ -0,0 +1,2 @@
+wsl --update
+wsl --set-default-version 2
