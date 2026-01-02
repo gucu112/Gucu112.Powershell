ee7961a8793f75e8bd88d8f51ab58fb7fbb681ff
Bartlomiej Roszczypala <gucu112@hotmail.com>
2026-01-02 19:58:21 +0100

    Add Win11Debloat with configuration

--- others/WindowsInstallation/0__DuringInstall.bat
+++ others/WindowsInstallation/0__DuringInstall.bat
@@ -2,11 +2,8 @@ REM Bypass network requirements
 
 OOBE\BYPASSNRO
 
-REM Disable security questions
-REM TODO: Test if it works during installation
-
-REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\System /v NoLocalPasswordResetQuestions /t REG_DWORD /d 1 /f
-
 REM Disable internet connection
 
 IPCONFIG /RELEASE
+
+REM Leave password blank to skip security questions
