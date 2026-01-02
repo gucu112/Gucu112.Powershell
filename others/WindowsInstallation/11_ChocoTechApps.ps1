d181313b5e170da84951a6cd2aaca66bacd4e6d4
Bartlomiej Roszczypala <gucu112@hotmail.com>
2026-01-02 20:00:20 +0100

    Remove unnecessary force flag when running install scripts

--- others/WindowsInstallation/11_ChocoTechApps.ps1
+++ others/WindowsInstallation/11_ChocoTechApps.ps1
@@ -1,4 +1,4 @@
-Invoke-Expression -Command "& .\modules\Gucu112.Powershell.PackageManagement\tools\InstallPackageManagement.ps1 -Force:`$false"
+Invoke-Expression -Command "& .\modules\Gucu112.Powershell.PackageManagement\tools\InstallPackageManagement.ps1"
 
 # CPlusPlus & CSharp
 @('vcredist-all', 'directx', 'dotnetfx', 'dotnet-sdk') | ForEach-Object {
--- others/WindowsInstallation/12_LenovoDriversApps.ps1
+++ others/WindowsInstallation/12_LenovoDriversApps.ps1
@@ -1,4 +1,4 @@
-Invoke-Expression -Command "& .\modules\Gucu112.Powershell.PackageManagement\tools\InstallWinGetClient.ps1 -Force:`$false"
+Invoke-Expression -Command "& .\modules\Gucu112.Powershell.PackageManagement\tools\InstallWinGetClient.ps1"
 
 @('Lenovo.SystemUpdate', 'Lenovo.ServiceBridge') | ForEach-Object {
     Install-WinGetPackage -Id $PSItem -MatchOption Equals -Source winget -Mode Silent
